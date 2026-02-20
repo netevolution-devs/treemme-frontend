f635538d2059cbc91c09a16436e8dddd7d3a7b7d
Developer 3 <dev3@netevolution.it>
2026-02-20 10:04:57 +0100

    feat(ui): add dynamic theming to PanelContainerPage based on material UI theme

--- src/features/panels/PanelContainerPage.tsx
+++ src/features/panels/PanelContainerPage.tsx
@@ -1,16 +1,25 @@
-import {Box} from "@mui/material";
+import {Box, useTheme} from "@mui/material";
 import {PANEL_REGISTRY} from "@features/panels/PanelRegistry.tsx";
 import {useDockviewStore} from "@ui/panel/store/DockviewStore.ts";
 import {DockviewReact} from "dockview";
 import {StyledDockviewWrapper} from "@themes/dockviewTheme.tsx";
+import {themeLight} from "dockview";
+import {themeDark} from "dockview";
 
 const PanelContainerPage = () => {
     const {handleReady} = useDockviewStore(state => state);
+    const materialTheme = useTheme();
+
+    const isLightTheme = materialTheme.palette.mode === 'light';
 
     return (
         <Box height="calc(100vh - 48px)">
             <StyledDockviewWrapper>
                 <DockviewReact
+                    theme={isLightTheme
+                        ? themeLight
+                        : themeDark
+                    }
                     className="dockview-theme-mui"
                     components={PANEL_REGISTRY}
                     onReady={handleReady}
