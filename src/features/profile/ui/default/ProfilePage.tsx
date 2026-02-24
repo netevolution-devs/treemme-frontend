2f41a2633a49dd84d845617e87183d1abc333417
Marco Egidio <developer@net-evolution.com>
2026-02-24 16:43:47 +0100

    refactor: remove unused imports and commented code in ProfilePage for cleaner structure

--- src/features/profile/ui/default/ProfilePage.tsx
+++ src/features/profile/ui/default/ProfilePage.tsx
@@ -1,27 +1,28 @@
-import {useAuth} from "@features/auth/model/AuthContext.tsx";
 import BasePage from "@ui/layout/BasePage.tsx";
 import {useTranslation} from "react-i18next";
 import UserProfileCard from "./UserProfileCard.tsx";
 import ButtonLogout from "@features/auth/ui/ButtonLogout.tsx";
 import {SettingsSection} from "@features/settings/ui/default/SettingsPage.tsx";
 import BaseSettingsContainer from "@ui/container/BaseSettingsContainer.tsx";
-import useGetProfile from "@features/profile/api/useGetProfile.ts";
-import UserProfileCardSkeleton from "@features/profile/ui/default/UserProfileCardSkeleton.tsx";
 import {Box} from "@mui/material";
+// import useGetProfile from "@features/profile/api/useGetProfile.ts";
+// import {useAuth} from "@features/auth/model/AuthContext.tsx";
+// import UserProfileCardSkeleton from "@features/profile/ui/default/UserProfileCardSkeleton.tsx";
 
 const ProfilePage = () => {
     const {t} = useTranslation(["settings"]);
-    const {user} = useAuth();
-    const {isLoading} = useGetProfile(user?.userCode as string);
+    // const {user} = useAuth();
+    // const {isLoading} = useGetProfile(user?.userCode as string);
 
     return (
         <BasePage sx={{p: 20, mb: 3}} fullwidth>
             <BaseSettingsContainer>
                 <SettingsSection title={t("profile.title")}>
-                    {isLoading
-                        ? <UserProfileCardSkeleton/>
-                        : <UserProfileCard/>
-                    }
+                    {/*{isLoading*/}
+                    {/*    ? <UserProfileCardSkeleton/>*/}
+                    {/*    : <UserProfileCard/>*/}
+                    {/*}*/}
+                    <UserProfileCard/>
                     <Box>
                         <ButtonLogout/>
                     </Box>
