20dfe61eb92cbd32d582bd9990bca1a7d9b5f3fb
Marco Egidio <developer@net-evolution.com>
2026-02-24 10:58:52 +0100

    refactor: remove theme dependency from getMrtTableHeadCellProps

--- src/shared/ui/table/props/getMrtTableHeadCellProps.ts
+++ src/shared/ui/table/props/getMrtTableHeadCellProps.ts
@@ -1,13 +1,11 @@
-import type {TableCellProps as MUITableCellProps, Theme} from "@mui/material";
+import type {TableCellProps as MUITableCellProps} from "@mui/material";
 
-export const getMrtTableHeadCellProps = (theme: Theme): MUITableCellProps => ({
+export const getMrtTableHeadCellProps = (): MUITableCellProps => ({
     sx: {
         margin: 0,
         paddingLeft: 1,
         paddingTop: 0.5,
         paddingBottom: 0.5,
-        backgroundColor: theme.palette.mode === "light" ? theme.palette.primary.light : theme.palette.action.hover,
-        color: theme.palette.mode === "light" ? theme.palette.primary.contrastText : theme.palette.primary.main,
         fontWeight: 600,
         border: 'none'
     }
