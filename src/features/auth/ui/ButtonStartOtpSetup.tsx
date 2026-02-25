fae0f888d2b15f48019e669f77a3ca43b48c307c
Marco Egidio <developer@net-evolution.com>
2026-02-25 14:37:24 +0100

    feat: add `ButtonStartOtpSetup` component and translations for OTP setup

---
+++ src/features/auth/ui/ButtonStartOtpSetup.tsx
@@ -0,0 +1,32 @@
+import {Button} from "@mui/material";
+import {useNavigate} from "react-router";
+import type {ButtonProps as MuiButtonProps} from "@mui/material";
+import {useTranslation} from "react-i18next";
+import {useLayout} from "@ui/layout/default/LayoutContext.tsx";
+
+interface Props extends MuiButtonProps {
+    label?: string,
+}
+
+const ButtonStartOtpSetup = ({label, ...props}: Props) => {
+    const {t} = useTranslation(["settings"]);
+    const {setShowTopBar} = useLayout()
+    const navigate = useNavigate();
+
+    const handleStartOtpSetup = () => {
+        setShowTopBar(false);
+        navigate("/login/otp/setup");
+    }
+
+    return (
+        <Button
+            onClick={handleStartOtpSetup}
+            variant={"outlined"}
+            {...props}
+        >
+            {label ?? t("interface.go-to-setup-otp")}
+        </Button>
+    )
+}
+
+export default ButtonStartOtpSetup;
