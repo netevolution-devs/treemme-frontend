ed025404835fb4e80f63f763199436332d38bd0b
Marco Egidio <developer@net-evolution.com>
2026-03-26 11:38:03 +0100

    feat: extend IUserManagement API with group and group user interfaces

--- src/features/panels/user-management/api/IUserManagement.ts
+++ src/features/panels/user-management/api/IUserManagement.ts
@@ -1,8 +1,19 @@
+export interface IUserManagementGroup {
+    id: number;
+    name: string;
+    description: string | null;
+}
+
+export interface IUserManagementGroupUser {
+    group: IUserManagementGroup;
+}
+
 export interface IUserManagement {
     id: number;
     user_code: string;
     email: string;
     last_access: string | null;
+    group_users: IUserManagementGroupUser[];
 }
 
 export interface IUserManagementPayload {
