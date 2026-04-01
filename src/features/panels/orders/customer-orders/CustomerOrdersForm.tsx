6b371f9700ad2101ac4d13a085bf2dc055275122
Developer 3 <dev3@netevolution.it>
2026-04-01 10:09:13 +0200

    feat(panels): integrate dynamic client selection panel in CustomerOrdersForm

--- src/features/panels/orders/customer-orders/CustomerOrdersForm.tsx
+++ src/features/panels/orders/customer-orders/CustomerOrdersForm.tsx
@@ -26,6 +26,8 @@ import type {
 import {
     shipmentConditionApi
 } from "@features/panels/orders/customer-orders/api/shipment-condition/shipmentConditionApi.ts";
+import useCallablePanel from "@ui/panel/useCallablePanel.ts";
+import useSubscribePanel from "@ui/panel/useSubscribePanel.ts";
 
 export type ICustomerOrderForm = Omit<ICustomerOrder, "id"
     | "client"
@@ -94,6 +96,13 @@ const FormFields = ({clients, payments, shipmentConditions, order, selectedCusto
             .join(', ');
     };
 
+    const {add: addSelectPanel} = useCallablePanel();
+
+    useSubscribePanel<ICustomerOrderForm>({
+        formKey: "client_id",
+        dependencyKey: "contacts"
+    })
+
     return (
         <>
             <Box sx={{display: 'flex', flexDirection: 'row', gap: 1}}>
@@ -135,6 +144,18 @@ const FormFields = ({clients, payments, shipmentConditions, order, selectedCusto
                     label={t("orders.client")}
                     options={clients.map(c => ({value: c.id, label: c.name}))}
                     required
+                    onNoOptionsMatch={(input) => {
+                        addSelectPanel({
+                            extra: {
+                                client: true
+                            },
+                            initialValue: input,
+                            menu: {
+                                component: "contacts",
+                                i18nKey: "menu.contacts.contacts"
+                            }
+                        })
+                    }}
                 />
                 <TextFieldControlled<ICustomerOrderForm>
                     label={t("orders.client_order_number")}
