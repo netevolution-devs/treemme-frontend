434b106637cf1b865d43830638266c4aa7f6d393
Marco Egidio <developer@net-evolution.com>
2026-04-01 16:41:10 +0200

    feat: enhance menu entries with permission checks and add strict typing for resource keys in permissions utils

--- src/features/authz/permission.utils.ts
+++ src/features/authz/permission.utils.ts
@@ -2,16 +2,101 @@ import type {IAccessControl, IRoles} from "@features/user/model/RoleInterfaces.t
 
 export type RoleCheckMode = "any" | "all";
 export type ResourceAction = "get" | "post" | "put" | "delete";
-export type ResourceName = "contatti"
-    | "pellami"
-    | "prodotti"
-    | "ordini"
-    | "magazzino"
-    | "produzione"
-    | "ddt & fatture"
-    | "commerciale"
-    | "analisi"
-    | "sistema"
+export type ResourceName = ResourceContacts
+    | ResourceLeathers
+    | ResourceProducts
+    | ResourceOrders
+    | ResourceWarehouse
+    | ResourceProduction
+    | ResourceShippingInvoicing
+    | ResourceCommercial
+    | ResourceAnalysis
+    | ResourceSystem
+
+export type ResourceContacts = "contatti"
+    | "contatti - contatti"
+    | "contatti - cap"
+    | "contatti - nazioni"
+    | "contatti - province"
+    | "contatti - porti marittimi"
+
+export type ResourceLeathers = "pellami"
+    | "pellami - pellami"
+    | "pellami - pesi"
+    | "pellami - specie"
+    | "pellami - taglie"
+    | "pellami - spessori"
+    | "pellami - tipologie"
+    | "pellami - scuoiature"
+    | "pellami - provenienze"
+    | "pellami - stati di lavorazione"
+
+export type ResourceProducts = "prodotti"
+    | "prodotti - prodotti"
+    | "prodotti - articoli"
+    | "prodotti - scelte"
+    | "prodotti - ricerca prodotti"
+    | "prodotti - categorie"
+    | "prodotti - tipologie prodotti"
+    | "prodotti - tipologie articoli"
+
+export type ResourceOrders = "ordini"
+    | "ordini - ordini clienti"
+    | "ordini - ordini fornitori"
+    | "ordini - ricerca ordini clienti"
+    | "ordini - scadenziario fornitori"
+    | "ordini - ricerca ordini fornitori"
+
+export type ResourceWarehouse = "magazzino"
+    | "magazzino - movimenti di magazzino"
+    | "magazzino - tipologie pallets"
+    | "magazzino - lotti e partite"
+    | "magazzino - ricerca lotti/partite"
+    | "magazzino - lotti in conto lavorazione"
+
+export type ResourceProduction = "produzione"
+    | "produzione - lotti"
+    | "produzione - ricette"
+    | "produzione - macchinari"
+    | "produzione - avanzamento produzione"
+    | "produzione - produzione"
+    | "produzione - lavorazioni"
+    | "produzione - distinta base"
+    | "produzione - scelta partite"
+    | "produzione - monitor pirovano"
+    | "produzione - ricettario pirovano"
+
+export type ResourceShippingInvoicing = "ddt & fatture"
+    | "ddt & fatture - documenti di trasporto"
+    | "ddt & fatture - fatturazione"
+    | "ddt & fatture - ricerca documenti"
+    | "ddt & fatture - ragioni di trasporto"
+    | "ddt & fatture - lotti conto lavoro"
+
+export type ResourceCommercial = "commerciale"
+    | "commerciale - anagrafica clienti"
+    | "commerciale - anagrafica fornitori"
+    | "commerciale - dati partite"
+    | "commerciale - costo prodotti"
+    | "commerciale - listino prodotti"
+    | "commerciale - rapportini di lavoro"
+    | "commerciale - valute e cambi"
+
+export type ResourceAnalysis = "analisi"
+    | "analisi - analisi ordini"
+    | "analisi - analisi vendite"
+    | "analisi - costi e ricavi"
+    | "analisi - analisi partite/lotti"
+    | "analisi - analisi conto lavorazione"
+    | "analisi - traffico telefonico"
+    | "analisi - controllo qualità iso"
+    | "analisi - controllo qualità iso - evasione ordini"
+
+export type ResourceSystem = "sistema"
+    | "sistema - informazioni di sistema"
+    | "sistema - strumenti"
+    | "sistema - console di comando"
+    | "sistema - gestione accessi"
 
 function checkRoles(set: Set<IRoles>, roles: IRoles[], mode: RoleCheckMode = "all") {
     if (roles.length === 0) return mode === "all";
