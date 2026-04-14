dcdc06d1fb4aeeaf5ecdae316a994f1083895986
m.egidio <developer@net-evolution.com>
2026-04-14 15:27:00 +0200

    feat(routing): preserve original location when redirecting unauthenticated users to login

--- src/features/routing/AuthUserGuard.tsx
+++ src/features/routing/AuthUserGuard.tsx
@@ -1,16 +1,17 @@
-import {Navigate, Outlet} from "react-router";
+import {Navigate, Outlet, useLocation} from "react-router";
 import {AuthFetchingScreen} from "./Fallbacks";
 import {useAuth} from "@features/auth/model/AuthContext";
 
 const AuthUserGuard = () => {
     const {isAuthenticated, isLoading} = useAuth();
+    const location = useLocation();
 
     if (isLoading) {
         return <AuthFetchingScreen/>;
     }
 
     if (!isAuthenticated) {
-        return <Navigate to="/login" replace/>;
+        return <Navigate to="/login" state={{from: location}} replace/>;
     }
 
     return <Outlet/>
