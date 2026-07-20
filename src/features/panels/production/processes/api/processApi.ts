5d197e40b38a0450e71a1d15b3b4d2912caed1c4
Developer 3 <dev3@netevolution.it>
2026-07-20 14:11:22 +0200

    fix(panels): ensure file name in export removes `.csv` before appending date suffix

--- src/features/panels/production/processes/api/processApi.ts
+++ src/features/panels/production/processes/api/processApi.ts
@@ -1,7 +1,7 @@
 import {createPanelApi} from "@features/panels/shared/hooks/createPanelApiFactory";
 import type {IProcess} from "@features/panels/production/processes/api/IProcess";
-import useApi from "@api/useApi";
 import {useMutation} from "@tanstack/react-query";
+import {useTranslation} from "react-i18next";
 
 export const processApi = {
     ...createPanelApi<IProcess>({
@@ -9,18 +9,11 @@ export const processApi = {
         queryKey: "PROCESS"
     }),
     useGetDailyPrint: () => {
-        const {get} = useApi();
+        const {i18n} = useTranslation();
         return useMutation({
             mutationKey: ["DAILY-PRINT-PDF-PRINT"],
             mutationFn: async (date: string) => {
-                const response = await get<Blob>("/production/daily-print", {
-                    params: {date},
-                    responseType: "blob",
-                });
-                const blob = new Blob([response.data as unknown as BlobPart], {type: "application/pdf"});
-                const url = window.URL.createObjectURL(blob);
-                window.open(url, "_blank");
-                return url;
+                window.open(`${import.meta.env.VITE_API}/production/daily-print?lang=${i18n.language || 'it'}&date=${date}`, "_blank");
             }
         })
     },
